bd83a1cba0907d12e6400afaad134395f386744f
Matthew Broughton <hello@matt-broughton.com>
2024-08-29 08:49:13 +1000

    Update Project Template
    Update Project Template to handle rendering blocks and such

--- src/components/templates/ProjectTemplate.tsx
+++ src/components/templates/ProjectTemplate.tsx
@@ -1,15 +1,35 @@
+'use client'
 import React from 'react'
+import { useLivePreview } from '@payloadcms/live-preview-react'
+
 import { Project } from '@types'
+
+import PageHead from '@components/organisms/PageHead'
+import Main from '@components/atoms/Main'
+import RenderBlocks from '@components/organisms/RenderBlocks'
 
 type ProjectTemplateProps = {
 	project: Project
 }
 
 const ProjectTemplate: React.FC<ProjectTemplateProps> = ({ project }) => {
+	const { data } = useLivePreview({
+		serverURL: process.env.NEXT_PUBLIC_PAYLOAD_SERVER_URL || '',
+		depth: 1,
+		initialData: project,
+	})
+	const { layout, pageHead } = data
 	return (
 		<div>
-			<h1>{project.title}</h1>
-			{/* Add more project-specific fields here */}
+			<PageHead {...pageHead} />
+			<Main className="p-5">
+				{layout && (
+					<RenderBlocks
+						className="space-y-20 tablet-landscape:space-y-24 laptop:space-y-30"
+						layout={layout}
+					/>
+				)}
+			</Main>
 		</div>
 	)
 }
